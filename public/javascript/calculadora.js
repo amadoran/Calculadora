0c05185238ffb422695de468fbd3c0cf93c37117
amadoran <afamador@espol.edu.ec>
2023-06-18 23:50:46 -0500

    finalizado

--- public/javascript/calculadora.js
+++ public/javascript/calculadora.js
@@ -6,8 +6,21 @@ let funcionPredeterminada = () =>{
 
         let interfazHoras = parseFloat(document.getElementById("interfazHoras").value)
         let interfazHTML = parseFloat(document.getElementById("interfazHTML").value)
+        let interfazJS = parseFloat(document.getElementById("interfazJS").value)
+        let interfazTest = parseFloat(document.getElementById("interfazTest").value)
 
-        let resultado = (valorPorHora * interfazHoras) + (valorPorHora * interfazHTML)
+        let checkOscuro = document.getElementById("modoOscuro").checked
+        let estilo = document.getElementById("estilo").value
+
+        let resultado = valorPorHora * (interfazHoras+interfazHTML+interfazJS+interfazTest)
+
+        if(checkOscuro){
+            resultado += 0.5 * (interfazHoras + interfazHTML)
+        }
+
+        if(estilo === "premium"){
+            resultado += 20
+        }
 
         document.getElementById("valorTotal").value = resultado
     })
